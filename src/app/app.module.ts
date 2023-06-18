09be1f5f2045d40253f89fa19461e6642e78adbb
shruthihsrinath <20953854+shruthihsrinath@users.noreply.github.com>
2023-06-17 23:14:40 -0400

    First Iteration completed (#2)
    * First Iteration completed - Includes the following
     - Created a game card component.
     - Created a header component.
    
    * First Iteration corrections as per comments
    
    ---------
    
    Co-authored-by: shruthihsrinath <shruthihsrinath@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { GameCardComponent } from './game-card/game-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    GameCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/game-card/game-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'game-card',
+  templateUrl: './game-card.component.html',
+  styleUrls: ['./game-card.component.css']
+})
+export class GameCardComponent {
+
+}
