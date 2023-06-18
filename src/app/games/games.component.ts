94d3fa5d5dc6c543182e967ddef27ed7e5f7ad1d
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-06-18 01:45:54 -0400

    Create about and game detail page and an app router page for routes Change in media query for mobile devices

---
+++ src/app/games/games.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'games',
+  templateUrl: './games.component.html',
+  styleUrls: ['./games.component.css']
+})
+export class GamesComponent {
+
+}
