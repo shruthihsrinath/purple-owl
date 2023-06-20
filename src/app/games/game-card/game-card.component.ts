d61d7a425242cdb9912e7a025699bcdc63fc87f7
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-06-20 00:36:16 -0400

    Second Iteration - Created Button component and added color palette

--- src/app/games/game-card/game-card.component.ts
+++ src/app/games/game-card/game-card.component.ts
@@ -7,8 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./game-card.component.css']
 })
 export class GameCardComponent {
+  gameName: string = "Aurum";
+  aurumButton: string = "More on aurum";
+  gameUrl: string = 'assets/aurum.png';
+  gameSummary: string = `Aurum is a trick-taking game for three or four players, with two teams of two competing in the four-player
+  game. After all cards for the round have been dealt, you bid on how many tricks you think you/your team will win.
+  (In a four-player game, the higher of the two bids on a team becomes the team's bid.)`;
   constructor(private router: Router) { }
-  readMore() {
-    this.router.navigate(['/games/game-detail']);
+  readMore(clicked: boolean) {
+    if (clicked) {
+      this.router.navigate(['/games/game-detail']);
+    }
   }
 }
