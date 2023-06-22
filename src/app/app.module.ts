6c4c61f414568f378869d2908deb09bd92d91dd3
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-06-22 02:21:48 -0400

    Create Service to read json file, update router to pass data properly through the pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+<<<<<<< HEAD
+=======
+import { GamesComponent } from './games/games.component';
+import { GameCardComponent } from './games/game-card/game-card.component';
+import { AboutComponent } from './about/about.component';
+import { GameDetailComponent } from './games/game-detail/game-detail.component';
+import { ButtonComponent } from './shared/button/button.component';
+>>>>>>> 5e749b4 (created Service to read json file and updated router to route and pass data properly to all the pages)
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
