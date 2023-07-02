34e1d3baee031be7121bcb5fbbdf9d6ff7fd07ed
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-07-02 01:24:18 -0400

    Fix second-iteration pull request Modify GameCardComponent to button for accessibility. Add observable for window resize. Rename blog model for better understanding and simplicity.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
+=======
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { PagesComponent } from './pages/pages.component';
+>>>>>>> 764c185 (Fix second-iteration pull request)
 
 @NgModule({
   declarations: [
