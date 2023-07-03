7adb511b93d74c28fce7bedd09e9b05ea8296934
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-07-03 00:40:03 -0400

    Fix second iteration pull request Lazy load pages module Capitalize all alt text Prepend all component selectors with po Remove comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
