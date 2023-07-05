2d30dc56069d70e6378d895598367f2308efdc88
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-07-05 14:37:29 -0400

    Website styling - Dynamically upload website background image - Modify routes for all the pages - Restyle the website

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, Event } from '@angular/router';
+<<<<<<< HEAD
 import { filter } from 'rxjs/operators';
+=======
+>>>>>>> 0bbba26 (Website styling)
 
 
 @Component({
@@ -11,15 +14,26 @@ import { filter } from 'rxjs/operators';
 export class PagesComponent {
   noBackground: boolean = false;
   constructor(private router: Router) {
+<<<<<<< HEAD
     // change the background when it route's to home
     this.router.events.pipe(filter((e: Event | NavigationEnd): e is NavigationEnd => e instanceof NavigationEnd))
       .subscribe((e: NavigationEnd) => {
         if (e.urlAfterRedirects == '/pages/home') {
+=======
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url == '/pages/home') {
+>>>>>>> 0bbba26 (Website styling)
           this.noBackground = true;
         }
         else {
           this.noBackground = false;
         }
+<<<<<<< HEAD
       });
+=======
+      }
+    })
+>>>>>>> 0bbba26 (Website styling)
   }
 }
