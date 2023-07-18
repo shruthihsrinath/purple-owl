8429c12893e340ff6506653c1e378dbe90abc2fa
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-07-17 21:55:40 -0400

    Modified home page

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 
 @Component({
@@ -10,15 +11,15 @@ import { NavigationEnd, Router, Event } from '@angular/router';
 export class PagesComponent {
   noBackground: boolean = false;
   constructor(private router: Router) {
-    this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationEnd) {
-        if (event.url == '/pages/home') {
+    // change the background when it route's to home
+    this.router.events.pipe(filter((e: Event | NavigationEnd): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        if (e.urlAfterRedirects == '/pages/home') {
           this.noBackground = true;
         }
         else {
           this.noBackground = false;
         }
-      }
-    })
+      });
   }
 }
