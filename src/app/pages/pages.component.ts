91ebb8a8c4a81489276205b399330b9b2bf8109e
shruthihsrinath <shruthihsrinath@users.noreply.github.com>
2023-07-17 21:55:40 -0400

    Modified home page

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, Event } from '@angular/router';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { filter } from 'rxjs/operators';
 =======
 >>>>>>> 0bbba26 (Website styling)
+=======
+import { filter } from 'rxjs/operators';
+>>>>>>> 8429c12 (Modified home page)
 
 
 @Component({
@@ -15,25 +19,35 @@ export class PagesComponent {
   noBackground: boolean = false;
   constructor(private router: Router) {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 8429c12 (Modified home page)
     // change the background when it route's to home
     this.router.events.pipe(filter((e: Event | NavigationEnd): e is NavigationEnd => e instanceof NavigationEnd))
       .subscribe((e: NavigationEnd) => {
         if (e.urlAfterRedirects == '/pages/home') {
+<<<<<<< HEAD
 =======
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
         if (event.url == '/pages/home') {
 >>>>>>> 0bbba26 (Website styling)
+=======
+>>>>>>> 8429c12 (Modified home page)
           this.noBackground = true;
         }
         else {
           this.noBackground = false;
         }
 <<<<<<< HEAD
+<<<<<<< HEAD
       });
 =======
       }
     })
 >>>>>>> 0bbba26 (Website styling)
+=======
+      });
+>>>>>>> 8429c12 (Modified home page)
   }
 }
