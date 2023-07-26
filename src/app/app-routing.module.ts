94ee7254ec9c3570fb7fd8a0c2fe28f546ede0c2
shruthihsrinath <20953854+shruthihsrinath@users.noreply.github.com>
2023-07-25 22:45:06 -0400

    Feature add netlify (#4)
    * Create about and game detail page and an app router page for routes
    
    * Second Iteration - Create Button component and added color palette
    
    * Modify game details page
    create service to read data from json
    
    * Create Service to read json file
    Update router to route and pass data properly to all the pages
    
    * Restyle the pages
    Add click functionality for mobile resize
    
    * Fix second-iteration pull request
    Modify GameCardComponent to button for accessibility.
    Add observable for window resize.
    Rename blog model for better understanding and simplicity.
    
    * Fix second iteration pull request
    Lazy load pages module
    Capitalize all alt text
    Prepend all component selectors with po
    Remove comments
    
    * Resolve comments on pull request
    Rename methods appropriately
    Decouple css from html
    Rename blog service
    
    * Website styling
    - Dynamically upload website background image
    - Modify routes for all the pages
    - Restyle the website
    
    * Modified home page
    
    * Home Page modification
    - Modified Home Page
    - Modified Games Page links for mobile size
    
    * Create About and Game Detail pages and app router page for routes
    
    * Second Iteration - Created Button component and added color palette
    
    * Modify Game Detail page and create service to read data from json
    
    * Create Service to read json file, update router to pass data properly through the pages
    
    * Restyle the pages and add click functionality for mobile resize
    
    * Fix second-iteration pull request
    Modify GameCardComponent to button for accessibility.
    Add observable for window resize.
    Rename blog model for better understanding and simplicity.
    
    * Fix second iteration pull request
    Lazy load pages module
    Capitalize all alt text
    Prepend all component selectors with po
    Remove comments
    
    * Resolve comments on pull request
    Rename methods appropriately
    Decouple css from html
    Rename blog service
    
    * Resolved commit conflicts
    
    * Resolved  commit conflicts with main
    -minor css style changes
    
    * Resolved commit conflicts for Feature add Netlify
    - Typo correction
    
    * Resolve commit conflicts Feature-add-netlify
    - Typo correction
    
    ---------
    
    Co-authored-by: shruthihsrinath <shruthihsrinath@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
   {
     path: 'pages',
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 
 @Component({
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./pages.component.css']
 })
 export class PagesComponent {
-
+  noBackground: boolean = false;
+  constructor(private router: Router) {
+    // change the background when it route's to home
+    this.router.events.pipe(filter((e: Event | NavigationEnd): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        if (e.urlAfterRedirects == '/pages/home') {
+          this.noBackground = true;
+        }
+        else {
+          this.noBackground = false;
+        }
+      });
+  }
 }
